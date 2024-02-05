d33b6918614d595693ebfe24c3842f12876f27fe
bruceg14 <ziyuan.guo@vanderbilt.edu>
2024-02-04 22:15:07 -0500

    Spring Boot setup

---
+++ src/main/java/com/example/back_end/BackEndJhwApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndJhwApplication.class, args);
+	}
+
+}
